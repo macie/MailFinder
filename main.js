0715648d61e29129e00e7238a449956925ee1c08
Maciej Żok <macie@users.noreply.github.com>
2019-03-07 19:17:58 +0100

    Show mail list on the left

--- main.js
+++ main.js
@@ -19,15 +19,18 @@ var regex =/([A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+)/g;
 // Using match() to fetch for regular expression. Here is essential function of finding list of emails.
 var myMails = LoadedHTML.match(regex);
 let uniqueMails = [...new Set(myMails)];
-    
-// Easy to copy version of list of emails made by match().
-var text = uniqueMails.join('<br>');
 
-document.getElementsByTagName("body")[0].innerHTML = text;
-
-/*
-if(mails)
-{
-  console.log(mails);
+// show mail list on the left
+let list = document.createElement('ul');
+list.style.cssText = 'position: absolute;top: 20px;left: 10px;background: white;padding: 1em 2em;border: 1px black solid;border-radius: 2px;';
+for (let mail of uniqueMails) {
+    let item = document.createElement('li');
+    let content = document.createTextNode(mail);
+    item.append(content);
+    list.append(item);
 }
-  */
+document.body.append(list);
+
+// Easy to copy version of list of emails made by match().
+//var text = uniqueMails.join('<br>');
+//document.getElementsByTagName("body")[0].innerHTML = text;
