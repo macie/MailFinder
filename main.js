ebf9b4f48bf06c6932087368ef19097d9fcd6f2f
Maciej Żok <macie@users.noreply.github.com>
2019-03-07 18:53:03 +0100

    Refactoring

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // @grant    none
 // ==/UserScript==
 /*
-	The Mail Fetcher is a simple script to find email list on a website by using regular expressions.
+  The Mail Fetcher is a simple script to find email list on a website by using regular expressions.
   The code below is divided by section: to load page html, to use regular expression to find email list, to save in easy to copy form.
   Good luck and have fun.
 */
@@ -12,7 +12,6 @@
 
 // Loading entire HTML included in body of the page.
 var LoadedHTML = document.getElementsByTagName("body")[0].innerHTML;
-
 
 // Definition of the regular expression which represent email adress.
 var regex =/([A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+)/g;
@@ -22,14 +21,7 @@ var myMails = LoadedHTML.match(regex);
 let uniqueMails = [...new Set(myMails)];
     
 // Easy to copy version of list of emails made by match().
-//var listOfMails = myMails.split(",");
-var i;
-var text = "";
-for (i = 0; i < uniqueMails.length; i++) 
-{
-  text += uniqueMails[i] + "<br>";
-} 
-
+var text = uniqueMails.join('<br>');
 
 document.getElementsByTagName("body")[0].innerHTML = text;
 
